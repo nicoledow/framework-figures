d18d6b8e1c29044e7ee6b3441d36e61bca2ec66d
Nicole Dow <nikki.dow71@gmail.com>
2021-01-16 10:33:25 -0700

    update navbar state to reflect which frameworks are currently selected

--- src/Components/AppSideNav.js
+++ src/Components/AppSideNav.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Typography from '@material-ui/core/Typography';
+import React, { useState } from "react";
+import Nav from "react-bootstrap/Nav";
+import { Button, Typography } from "@material-ui/core";
 
 export default function AppSideNav() {
-    const frameworksList = ['React', 'Angular', 'Ember', 'Vue'];
+  const frameworksList = ["React", "Angular", "Ember", "Vue"];
+  const [selectedFrameworks, updateFrameworkSelections] = useState([]);
 
-    return(
-        <Nav className="flex-column">
-            {frameworksList.map(framework => {
-                return(
-                    <Nav.Item key={framework} justify={true}>
-                        <Typography variant={'h4'}>
-                            {framework}
-                        </Typography>
-                    </Nav.Item>
-                )
-            })}
-        </Nav>
-    );
+  // remove (deselect) framework if it's already been selected, or add it to list for comparison
+  const selectFrameworkForComparison = framework => {
+     if (selectedFrameworks.includes(framework)) {
+         const frameworkIdx = selectedFrameworks.indexOf(framework);
+         selectedFrameworks.splice(frameworkIdx, 1);
+         updateFrameworkSelections(selectedFrameworks);
+     } else {
+        updateFrameworkSelections([...selectedFrameworks, framework]);
+     }
+  };
+  
 
+  return (
+    <Nav className="flex-column">
+      <Typography variant={"h6"} color={"secondary"}>
+        Choose frameworks to compare:
+      </Typography>
+      {frameworksList.map((framework) => {
+        return (
+          <Nav.Item key={framework} justify={true}>
+            <Button onClick={(e) => selectFrameworkForComparison(framework)} color={"secondary"} selected={selectedFrameworks.includes(framework)}>
+              {framework}
+            </Button>
+          </Nav.Item>
+        );
+      })}
+      <Button>Compare</Button>
+    </Nav>
+  );
 }
