8a732b4adb7e26ebc5828d83b8c7d9afe472bfb4
Nicole Dow <nikki.dow71@gmail.com>
2021-01-21 16:39:19 -0700

    fix typo

--- src/Components/GetFrameworkRecommendationButton.js
+++ src/Components/GetFrameworkRecommendationButton.js
@@ -66,7 +66,7 @@ export default function GetFrameworkRecommendationButton(props) {
             's score, as they indicate more people improving the code.
           </p>
           <p>
-            In addition to this, a lower number of contributed to
+            In addition to this, a lower number of issues contributed to
             {recommendation}'s score.
           </p>
         </div>
